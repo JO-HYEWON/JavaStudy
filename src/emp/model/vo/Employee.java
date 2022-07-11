6096b0df121ba93b827a0b6c36dabed44d0a7afb
혜원 <@DESKTOP-1OHNNAR>
2022-07-11 12:36:40 +0900

    first commit

---
+++ src/emp/model/vo/Employee.java
@@ -0,0 +1,8 @@
+package emp.model.vo;
+
+public class Employee { // public 왜씀? 그냥?
+	public void printEmployee() { // public static void mian 이거랑 뭐가다름
+		System.out.println("안녕하세요, 조혜원입니다.");
+		System.out.println("서울삽니다.");
+	}
+}
